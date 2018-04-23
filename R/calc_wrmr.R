bf1fec2ef6cf96e4f95d299dbb7fe56a2b2d5ed3
tobiasdienlin <tobias.dienlin@uni-hohenheim.de>
2018-04-23 17:05:54 +0200

    - updated calc_wrmr

--- R/calc_wrmr.R
+++ R/calc_wrmr.R
@@ -2,7 +2,7 @@ calc_wrmr = function(object) {
   # manually calculates wrmr from fitted lavaan object
   
   numerator <- inspect(object, what='fit')["chisq"]
-  denominator <- length(results@SampleStats@WLS.obs[[1]])
+  denominator <- length(object@SampleStats@WLS.obs[[1]])
   wrmr <- sqrt(numerator / denominator)
   return(wrmr)
 }
