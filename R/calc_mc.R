1824ec3046b1fca9a2632dd02ce165c4cbe0a52b
tobiasdienlin <tobias.dienlin@uni-hohenheim.de>
2018-04-25 16:16:52 +0200

    - updates on calc_mc function

--- R/calc_mc.R
+++ R/calc_mc.R
@@ -1,29 +1,31 @@
-calc_mc <- function(
-  a_est, a_se, a_std, 
-  b_est, b_se, b_std,
-  c_est, c_se, c_std, 
-  d_est, d_se, d_std,
-  print = TRUE,
-  draws = 20000) {
+calc_mc <- function(object, print = TRUE, draws = 20000) {
   
   # calculates Monte Carlo interval for indirect effects
-  if(missing(c_est)) {
-    ab_distr <- (rnorm(draws, a_est, a_se) * rnorm(draws, b_est, b_se))
-    ab_std <- a_std * b_std
-    temp <- data.frame(est = (a_est * b_est), 
+  if(nrow(object) == 2) {
+    ab_distr <- (rnorm(draws, object[1, "est"], object[1, "se"]) * 
+                             rnorm(draws, object[2, "est"], object[2, "se"]))
+    ab_std <- object[1, "std"] * object[2, "std"]
+    
+    temp <- data.frame(est = (object[1, "est"] * object[2, "est"]), 
                        ll = quantile(ab_distr, .025), 
                        ul = quantile(ab_distr, .975), 
                        std = ab_std)
-  } else {
-    ab_distr <- (rnorm(draws, a_est, a_se) * rnorm(draws, b_est, b_se))
-    ab_std <- a_std * b_std
-    cd_distr <- (rnorm(draws, c_est, c_se) * rnorm(draws, d_est, d_se))
-    cd_std <- c_std * d_std
+  } else if (nrow(object) == 4) {
+    ab_distr <- (rnorm(draws, object[1, "est"], object[1, "se"]) * 
+                   rnorm(draws, object[2, "est"], object[2, "se"]))
+    ab_std <- object[1, "std"] * object[2, "std"]
+    
+    cd_distr <- (rnorm(draws, object[3, "est"], object[3, "se"]) * 
+                   rnorm(draws, object[4, "est"], object[4, "se"]))
+    cd_std <- object[3, "std"] * object[4, "std"]
+    
     abcd_distr <- ab_distr + cd_distr
-    temp <- data.frame(est = ((a_est * b_est) + (c_est * d_est)), 
-                         ll = quantile(abcd_distr, .025), 
-                         ul = quantile(abcd_distr, .975), 
-                         std = ab_std + cd_std)
+    
+    temp <- data.frame(est = ((object[1, "est"] * object[2, "est"]) + 
+                                (object[3, "est"] * object[4, "est"])), 
+                       ll = quantile(abcd_distr, .025), 
+                       ul = quantile(abcd_distr, .975), 
+                       std = ab_std + cd_std)
   } 
   row.names(temp) <- NULL
   if(isTRUE(print)) {
