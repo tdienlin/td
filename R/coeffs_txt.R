4f943d80102549e1430f4036fe9e508fd415a071
tobiasdienlin <tobias.dienlin@uni-hohenheim.de>
2018-04-27 10:42:28 +0200

    Added indirect effect to coeffs_txt

--- R/coeffs_txt.R
+++ R/coeffs_txt.R
@@ -1,15 +1,29 @@
-coeffs_txt <- function(object, selection, se = FALSE){
-  paste0("\\textit{b} ",
-         my_round(filter(object, label == selection)$est, "b_txt"),
-         ", 95% CI [",
-         my_round(filter(object, label == selection)$ll, 2),
-         ", ",
-         my_round(filter(object, label == selection)$ul, 2),
-         "], \\textit{z} ",
-         my_round(filter(object, label == selection)$z, "2_txt"),
-         ", \\textit{p} ",
-         my_round(filter(object, label == selection)$p, "p_txt"),
-         ", $\\beta$ ",
-         my_round(filter(object, label == selection)$std, "std_txt")
-  )
+coeffs_txt <- function(object, selection, se = FALSE, 
+                       indirect_effect = FALSE) {
+
+  if(isTRUE(indirect_effect)) {
+    paste0("\\textit{b} ",
+           my_round(filter(object, label == selection)$est, "b_txt"),
+           ", 95% CI [",
+           my_round(filter(object, label == selection)$ll, 2),
+           ", ",
+           my_round(filter(object, label == selection)$ul, 2),
+           "], $\\beta$ ",
+           my_round(filter(object, label == selection)$std, "std_txt")
+    )
+  } else {
+    paste0("\\textit{b} ",
+           my_round(filter(object, label == selection)$est, "b_txt"),
+           ", 95% CI [",
+           my_round(filter(object, label == selection)$ll, 2),
+           ", ",
+           my_round(filter(object, label == selection)$ul, 2),
+           "], \\textit{z} ",
+           my_round(filter(object, label == selection)$z, "2_txt"),
+           ", \\textit{p} ",
+           my_round(filter(object, label == selection)$p, "p_txt"),
+           ", $\\beta$ ",
+           my_round(filter(object, label == selection)$std, "std_txt")
+    )
+  }
 }
