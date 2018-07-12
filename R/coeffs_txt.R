e715b8c652d6bf0dca7ffead9ca81bdf99667dca
tobiasdienlin <tobias.dienlin@uni-hohenheim.de>
2018-07-12 10:48:12 +0200

    - coeffs_txt: Integrated dfs

--- R/coeffs_txt.R
+++ R/coeffs_txt.R
@@ -12,18 +12,20 @@ coeffs_txt <- function(object, selection, se = FALSE,
            my_round(filter(object, label == selection)$std, "std_txt")
     )
   } else {
-    paste0("\\textit{b} ",
+    paste0("$\\beta$ ",
+           my_round(filter(object, label == selection)$std, "std_txt"),
+           ", \\textit{b} ",
            my_round(filter(object, label == selection)$est, "b_txt"),
            ", 95% CI [",
            my_round(filter(object, label == selection)$ll, 2),
            ", ",
            my_round(filter(object, label == selection)$ul, 2),
-           "], \\textit{z} ",
+           "], \\textit{z}(",
+           filter(object, label == selection)$df,
+           ") ",
            my_round(filter(object, label == selection)$z, "2_txt"),
            ", \\textit{p} ",
-           my_round(filter(object, label == selection)$p, "p_txt"),
-           ", $\\beta$ ",
-           my_round(filter(object, label == selection)$std, "std_txt")
+           my_round(filter(object, label == selection)$p, "p_txt")
     )
   }
 }
