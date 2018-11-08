7a4ad54d92958d4eaecad87ae0618824a18d1f15
tobiasdienlin <tobias.dienlin@uni-hohenheim.de>
2018-11-08 14:12:44 +0100

    string_break: fixed function

--- R/string_break.R
+++ R/string_break.R
@@ -1,7 +1,7 @@
-string_break <- fun(object = NULL, length = 40) {
+string_break <- function(object, length = 40) {
   # function to break long strings into short ones
   # function by "Deer Hunter", https://stackoverflow.com/a/29847221/8118356
   
   tmp <- paste(strwrap(object, length), collapse="\n")
   return(tmp)
-  }
+}
