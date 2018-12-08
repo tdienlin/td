60e3b6c06432f333e2a11196ff3fd7b8f006523a
tobiasdienlin <tobias.dienlin@uni-hohenheim.de>
2018-12-08 18:13:31 +0100

    fit_tab: Added functionality "as_text"

--- R/fit_tab.R
+++ R/fit_tab.R
@@ -1,7 +1,8 @@
 fit_tab <- function(object, 
                     measures = c("chisq", "df", "pvalue", "cfi", 
                                  "tli", "rmsea", "srmr"),
-                    wrmr = FALSE
+                    wrmr = FALSE,
+                    as_text = FALSE
 ) {
   # extract typical fit measures from lavaan object
   
@@ -13,5 +14,15 @@ fit_tab <- function(object,
   } else{
     fit_tab <- c(fitMeasures(object, fit.measures = measures))
   }
+  
+  if(isTRUE(as_text)) {
+    fit_tab %<>%
+      t() %>% 
+      as.data.frame() %>% 
+      mutate_at(vars(chisq), funs(my_round(., "coeff"))) %>% 
+      mutate_at(vars(cfi, tli, rmsea, srmr), funs(my_round(., "beta"))) %>% 
+      mutate_at(vars(pvalue), funs(my_round(., "p")))
+  }
+  
   return(fit_tab)
 }
